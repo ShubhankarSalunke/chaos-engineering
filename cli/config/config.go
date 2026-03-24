a1caebe618d4334fc8511b21c2e8fddfef1c970f
Shubhankar Salunke <shubhankar.salunke@gmail.com>
2026-03-24 20:03:32 +0530

    config.go changed for hosted backend

--- cli/config/config.go
+++ cli/config/config.go
@@ -1,28 +1,12 @@
 package config
 
-import (
-	"encoding/json"
-	"os"
-)
-
 type Config struct {
 	ServerURL string `json:"server_url"`
 }
 
 func GetServerURL() string {
-
-	file, err := os.Open(os.Getenv("HOME") + "/.chaos/config.json")
-	if err != nil {
-		return "http://localhost:8000"
-	}
-	defer file.Close()
-
 	var cfg Config
-	json.NewDecoder(file).Decode(&cfg)
-
-	if cfg.ServerURL == "" {
-		return "http://localhost:8000"
-	}
-
+	cfg.ServerURL = "http://localhost:8000"
+	// cfg.ServerURL = "https://aws_gateway_link"
 	return cfg.ServerURL
 }
